b7a1e58b296c960b06519add901e7b95f4d13e0b
nach0522 <gowthami.chinthala@netcracker.com>
2026-05-01 13:31:39 +0530

    feat: project_rules for keycloak-oidc

--- mistral/config.py
+++ mistral/config.py
@@ -66,6 +66,17 @@ auth_type_opt = cfg.StrOpt(
     help=_('Authentication type (valid options: keystone, keycloak-oidc)')
 )
 
+auth_opts = [
+    cfg.StrOpt(
+        'project_rules',
+        help=_(
+            'Rules for resolving project_id from token.'
+            'Example: [{"type": "extract", "field": "iss", '
+            '"pattern": "*/realms/{value}"}]'
+        )
+    )
+]
+
 legacy_action_provider_opts = [
     cfg.BoolOpt(
         'load_action_plugins',
@@ -996,6 +1007,7 @@ ACTION_HEARTBEAT_GROUP = 'action_heartbeat'
 ACTION_LOGGING_GROUP = 'action_logging'
 CONTEXT_VERSIONING_GROUP = 'context_versioning'
 PROFILER_GROUP = profiler.list_opts()[0][0]
+AUTH_GROUP = 'auth'
 KEYCLOAK_OIDC_GROUP = "keycloak_oidc"
 YAQL_GROUP = "yaql"
 HEALTHCHECK_GROUP = 'healthcheck'
@@ -1041,6 +1053,7 @@ CONF.register_opts(kafka_notifications_opts, group=KAFKA_NOTIFICATION_GROUP)
 CONF.register_opts(headers_propagation_opts, group=HEADERS_PROP_GROUP)
 CONF.register_opts(coordination_opts, group=COORDINATION_GROUP)
 CONF.register_opts(profiler_opts, group=PROFILER_GROUP)
+CONF.register_opts(auth_opts, group=AUTH_GROUP)
 CONF.register_opts(keycloak_oidc_opts, group=KEYCLOAK_OIDC_GROUP)
 CONF.register_opts(yaql_opts, group=YAQL_GROUP)
 CONF.register_opts(healthcheck_opts, group=HEALTHCHECK_GROUP)
@@ -1095,6 +1108,7 @@ def list_opts():
         (COORDINATION_GROUP, coordination_opts),
         (EXECUTION_EXPIRATION_POLICY_GROUP, execution_expiration_policy_opts),
         (PROFILER_GROUP, profiler_opts),
+        (AUTH_GROUP, auth_opts),
         (KEYCLOAK_OIDC_GROUP, keycloak_oidc_opts),
         (YAQL_GROUP, yaql_opts),
         (HEALTHCHECK_GROUP, healthcheck_opts),
